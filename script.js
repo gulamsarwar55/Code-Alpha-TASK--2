a579349237c1f56b8d8770963e50867946d26848
MD GULAM SARWAR <120262909+gulamsarwar55@users.noreply.github.com>
2023-06-28 22:14:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var css = document.querySelector('h3');
+var color1 = document.querySelector('.color1');
+var color2  = document.querySelector('.color2');
+var body = document.querySelector('#gradient');
+function setGradient(){
+    'lineaar-gradient(to right,' + color1.value + ',' + color2.value +')';
+
+    css.textContent = body.getElementsByClassName.backgrounds + ';';
+}
